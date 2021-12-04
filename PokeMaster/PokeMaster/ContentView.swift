eeaa01117a6c7e17c6817e4a1136abf2d12b4a1b
蔡龙君 <cailongjun@huami.com>
2021-12-04 14:41:02 +0800

    init PokeMaster

---
+++ PokeMaster/PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by Taylor.CAI on 2021/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeMaster/PokeMaster/PokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeMasterApp.swift
+//  PokeMaster
+//
+//  Created by Taylor.CAI on 2021/12/4.
+//
+
+import SwiftUI
+
+@main
+struct PokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
